fd4fe66f44fb59ed4d23f872baa49bf66f3f79d8
Cauê <54822334+Caue-Aron@users.noreply.github.com>
2023-06-02 19:26:29 -0300

    Adicionado arquivos do commit anterior

--- src/backend/model/Cardapio.java
+++ src/backend/model/Cardapio.java
@@ -6,31 +6,35 @@ public class Cardapio {
 	private String nome;
 	private ArrayList<Produto> produtos = new ArrayList<Produto>();
 	private ArrayList<Cardapio> sessoes = new ArrayList<Cardapio>();
-	
+
 	public Cardapio(String nome) {
 		setNome(nome);
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void addProduto(Produto prod) {
+		for (Produto produto : produtos)
+			if (produto.getNome() == prod.getNome())
+				throw new IllegalArgumentException("Já existe um produto com esse nome");
+		
 		produtos.add(prod);
 	}
-	
+
 	public void addSessao(Cardapio sessao) {
 		sessoes.add(sessao);
 	}
-	
+
 	public void removeProduto(Produto prod) {
 		produtos.remove(prod);
 	}
-	
+
 	public void removeSessao(Cardapio sessao) {
 		sessoes.remove(sessao);
 	}
--- src/backend/model/Pedido.java
+++ src/backend/model/Pedido.java
@@ -3,26 +3,30 @@ package backend.model;
 import java.util.ArrayList;
 
 public class Pedido {
-	private String mesa;
-	
-	private ArrayList<Produto> produtos = new ArrayList<Produto>();
-	
+	private String nome;
+
+	private ArrayList<Produto> produtos;
+
 	public Pedido(String mesa) {
-		setMesa(mesa);
-	}
-	
-	public String getMesa() {
-		return mesa;
+		setNome(mesa);
+		produtos = new ArrayList<Produto>();
 	}
 
-	public void setMesa(String mesa) {
-		this.mesa = mesa;		
+	public void setNome(String nome) {
+		if (nome.length() < 4)
+			throw new IllegalArgumentException("Nome de cliente muito curto.");
+
+		this.nome = nome;
 	}
-	
+
+	public String getNome() {
+		return nome;
+	}
+
 	public void addIngrediente(Produto p) {
 		produtos.add(p);
 	}
-	
+
 	public void removeIngrediente(Produto p) {
 		produtos.remove(p);
 	}

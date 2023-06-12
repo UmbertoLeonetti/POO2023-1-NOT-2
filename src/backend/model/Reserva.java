6aed003038c386945ad83988e1b6ab7a8902846b
Cauê <54822334+Caue-Aron@users.noreply.github.com>
2023-06-12 19:34:45 -0300

    Ajustes nas classes Pedido e Reserva

--- src/backend/model/Reserva.java
+++ src/backend/model/Reserva.java
@@ -3,38 +3,27 @@ package backend.model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Reserva extends Pedido {
+public class Reserva implements Item {
 
 	private LocalDate data;
 	private LocalTime horario;
+	private String nomeCliente;
 	
-	public Reserva(String nome, int mesa, LocalDate data, LocalTime horario) {
-		
-		super(nome, mesa);
-		
+	public Reserva(String nome) {		
 		setDataHorario(data, horario);
-		
+		// implementar data atual
 	}
 	
 	public void setDataHorario(LocalDate data, LocalTime horario) {
-		
 		LocalDate hoje = LocalDate.now();
-		
-		if (data.isBefore(hoje)) {
-			
+		if (data.isBefore(hoje))
 			throw new IllegalArgumentException("Data não pode ser anterior.");
-			
-		} 
 		
-		if (data.isEqual(hoje) && horario.isBefore(LocalTime.now())) {
-			
+		if (data.isEqual(hoje) && horario.isBefore(LocalTime.now()))
 			throw new IllegalArgumentException("Horário não pode ser anterior.");
-			
-		}
 		
 		this.data = data;
 		this.horario = horario;
-		
 	} 
 	
 	public LocalDate getData() {
@@ -43,6 +32,11 @@ public class Reserva extends Pedido {
 	
 	public LocalTime getHorario() {
 		return horario;
+	}
+
+	@Override
+	public String getNome() {
+		return nomeCliente;
 	}
 
 }

93cb68b22e6683eb0b623fb5e9f1bb36a467f7b6
Cauê <54822334+Caue-Aron@users.noreply.github.com>
2023-06-19 11:10:29 -0300

    Adicionado função de remover cardápio

--- src/view/panels/CardapioPanel.java
+++ src/view/panels/CardapioPanel.java
@@ -46,10 +46,31 @@ public class CardapioPanel extends JPanel {
 	private Controller<Cardapio> cardapios;
 	private JFrame main;
 	private JButton btnSalvar;
-	private JButton btnSessao;
 	private JButton btnCancelar;
 	private JPanel panel;
 	private ArrayList<String> produtos;
+	
+	private void removeCardapio() {
+		ArrayList<String> selected = (ArrayList<String>) listCard.getSelectedValuesList();
+		
+		int opcao;
+		int tamanho = selected.size();
+		
+		if (tamanho == 1)
+			opcao = JOptionPane.showConfirmDialog(panel, "Você realmente deseja excluir o prato \"" + selected.get(0) + "\"?");
+		else
+			opcao = JOptionPane.showConfirmDialog(panel, "Você realmente deseja excluir os " + tamanho + " pratos selecionados?");
+
+		if (opcao != 0)
+			return;
+	
+		for (String string : selected)
+			cardapios.remove(string);
+		
+		atualizaLista(cardapios.getNomes(), listCard);
+		limpaSelecao();
+		Persiste.salva(restaurante, "restaurante.txt");
+	}
 	
 	private void limpaSelecao() {
 		listCard.clearSelection();
@@ -70,10 +91,7 @@ public class CardapioPanel extends JPanel {
 		list.setModel(model);
 	}
 	
-	private void mudaSalvarCancelar(boolean acao) {
-		if(btnSessao != null)
-			btnSessao.setEnabled(acao);
-		
+	private void mudaSalvarCancelar(boolean acao) {		
 		btnSalvar.setEnabled(acao);
 		btnCancelar.setEnabled(acao);
 	}
@@ -136,243 +154,6 @@ public class CardapioPanel extends JPanel {
 		}
 		atualizaLista(card.getProdutos().getNomes(), listCardProd);
 		Persiste.salva(restaurante, "restaurante.txt");
-	}
-	
-	public CardapioPanel(JFrame main, final Restaurante restaurante, Cardapio cardapio) {
-		this.cardapio = cardapio;
-		this.restaurante = restaurante;
-		this.main = main;
-		
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		setLayout(gridBagLayout);
-		
-		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 0;
-		add(panel, gbc_panel);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
-		
-		JPanel pnlInfo = new JPanel();
-		GridBagConstraints gbc_pnlInfo = new GridBagConstraints();
-		gbc_pnlInfo.insets = new Insets(5, 5, 5, 5);
-		gbc_pnlInfo.fill = GridBagConstraints.BOTH;
-		gbc_pnlInfo.gridx = 0;
-		gbc_pnlInfo.gridy = 0;
-		panel.add(pnlInfo, gbc_pnlInfo);
-		GridBagLayout gbl_pnlInfo = new GridBagLayout();
-		gbl_pnlInfo.columnWidths = new int[]{147, 384, 0};
-		gbl_pnlInfo.rowHeights = new int[]{0, 0};
-		gbl_pnlInfo.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		gbl_pnlInfo.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		pnlInfo.setLayout(gbl_pnlInfo);
-		
-		JPanel panel_2 = new JPanel();
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.insets = new Insets(0, 0, 0, 5);
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 0;
-		pnlInfo.add(panel_2, gbc_panel_2);
-		GridBagLayout gbl_panel_2 = new GridBagLayout();
-		gbl_panel_2.columnWidths = new int[]{0, 0};
-		gbl_panel_2.rowHeights = new int[]{0, 0, 0};
-		gbl_panel_2.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel_2.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-		panel_2.setLayout(gbl_panel_2);
-		
-		JPanel panel_3 = new JPanel();
-		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
-		gbc_panel_3.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_3.fill = GridBagConstraints.BOTH;
-		gbc_panel_3.gridx = 0;
-		gbc_panel_3.gridy = 0;
-		panel_2.add(panel_3, gbc_panel_3);
-		GridBagLayout gbl_panel_3 = new GridBagLayout();
-		gbl_panel_3.columnWidths = new int[]{0, 0};
-		gbl_panel_3.rowHeights = new int[]{0, 0};
-		gbl_panel_3.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel_3.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		panel_3.setLayout(gbl_panel_3);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 0;
-		gbc_scrollPane.gridy = 0;
-		panel_3.add(scrollPane, gbc_scrollPane);
-		
-		JList list = new JList();
-		scrollPane.setViewportView(list);
-		
-		JPanel panel_5 = new JPanel();
-		GridBagConstraints gbc_panel_5 = new GridBagConstraints();
-		gbc_panel_5.fill = GridBagConstraints.BOTH;
-		gbc_panel_5.gridx = 0;
-		gbc_panel_5.gridy = 1;
-		panel_2.add(panel_5, gbc_panel_5);
-		GridBagLayout gbl_panel_5 = new GridBagLayout();
-		gbl_panel_5.columnWidths = new int[]{0, 0, 0};
-		gbl_panel_5.rowHeights = new int[]{0, 0};
-		gbl_panel_5.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_5.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel_5.setLayout(gbl_panel_5);
-		
-		JButton btnNewButton_2 = new JButton("Adicionar");
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_2.gridx = 0;
-		gbc_btnNewButton_2.gridy = 0;
-		panel_5.add(btnNewButton_2, gbc_btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("Excluir");
-		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
-		gbc_btnNewButton_3.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton_3.gridx = 1;
-		gbc_btnNewButton_3.gridy = 0;
-		panel_5.add(btnNewButton_3, gbc_btnNewButton_3);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(new LineBorder(new Color(128, 128, 128)), "Novo", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 1;
-		gbc_panel_1.gridy = 0;
-		pnlInfo.add(panel_1, gbc_panel_1);
-		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 0};
-		gbl_panel_1.rowHeights = new int[]{0, 0, 0};
-		gbl_panel_1.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-		panel_1.setLayout(gbl_panel_1);
-		
-		JPanel panel_4 = new JPanel();
-		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
-		gbc_panel_4.insets = new Insets(0, 5, 5, 5);
-		gbc_panel_4.fill = GridBagConstraints.BOTH;
-		gbc_panel_4.gridx = 0;
-		gbc_panel_4.gridy = 0;
-		panel_1.add(panel_4, gbc_panel_4);
-		GridBagLayout gbl_panel_4 = new GridBagLayout();
-		gbl_panel_4.columnWidths = new int[]{0, 0, 0};
-		gbl_panel_4.rowHeights = new int[]{0, 0, 0};
-		gbl_panel_4.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_4.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		panel_4.setLayout(gbl_panel_4);
-		
-		JLabel lblNewLabel = new JLabel("Nome:");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 0;
-		panel_4.add(lblNewLabel, gbc_lblNewLabel);
-		
-		tfNome = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 0);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 0;
-		panel_4.add(tfNome, gbc_textField);
-		tfNome.setColumns(10);
-		
-		JPanel panel_7 = new JPanel();
-		GridBagConstraints gbc_panel_7 = new GridBagConstraints();
-		gbc_panel_7.gridwidth = 2;
-		gbc_panel_7.fill = GridBagConstraints.BOTH;
-		gbc_panel_7.gridx = 0;
-		gbc_panel_7.gridy = 1;
-		panel_4.add(panel_7, gbc_panel_7);
-		GridBagLayout gbl_panel_7 = new GridBagLayout();
-		gbl_panel_7.columnWidths = new int[]{0, 0, 0, 0};
-		gbl_panel_7.rowHeights = new int[]{0, 0, 0};
-		gbl_panel_7.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_7.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		panel_7.setLayout(gbl_panel_7);
-		
-		JScrollPane scrollPane_2 = new JScrollPane();
-		GridBagConstraints gbc_scrollPane_2 = new GridBagConstraints();
-		gbc_scrollPane_2.gridheight = 2;
-		gbc_scrollPane_2.insets = new Insets(0, 0, 5, 5);
-		gbc_scrollPane_2.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane_2.gridx = 0;
-		gbc_scrollPane_2.gridy = 0;
-		panel_7.add(scrollPane_2, gbc_scrollPane_2);
-		
-		JList list_1 = new JList();
-		scrollPane_2.setViewportView(list_1);
-		
-		JButton btnNewButton_5 = new JButton(">>");
-		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
-		gbc_btnNewButton_5.anchor = GridBagConstraints.SOUTH;
-		gbc_btnNewButton_5.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_5.gridx = 1;
-		gbc_btnNewButton_5.gridy = 0;
-		panel_7.add(btnNewButton_5, gbc_btnNewButton_5);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
-		gbc_scrollPane_1.gridheight = 2;
-		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane_1.gridx = 2;
-		gbc_scrollPane_1.gridy = 0;
-		panel_7.add(scrollPane_1, gbc_scrollPane_1);
-		
-		JList list_2 = new JList();
-		scrollPane_1.setViewportView(list_2);
-		
-		JButton btnNewButton_6 = new JButton("<<");
-		GridBagConstraints gbc_btnNewButton_6 = new GridBagConstraints();
-		gbc_btnNewButton_6.anchor = GridBagConstraints.NORTH;
-		gbc_btnNewButton_6.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_6.gridx = 1;
-		gbc_btnNewButton_6.gridy = 1;
-		panel_7.add(btnNewButton_6, gbc_btnNewButton_6);
-		
-		JPanel panel_6 = new JPanel();
-		GridBagConstraints gbc_panel_6 = new GridBagConstraints();
-		gbc_panel_6.insets = new Insets(0, 5, 5, 5);
-		gbc_panel_6.fill = GridBagConstraints.BOTH;
-		gbc_panel_6.gridx = 0;
-		gbc_panel_6.gridy = 1;
-		panel_1.add(panel_6, gbc_panel_6);
-		GridBagLayout gbl_panel_6 = new GridBagLayout();
-		gbl_panel_6.columnWidths = new int[]{0, 0, 0, 0};
-		gbl_panel_6.rowHeights = new int[]{0, 0};
-		gbl_panel_6.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_6.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel_6.setLayout(gbl_panel_6);
-		
-		JButton btnNewButton = new JButton("Salvar");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton.gridx = 0;
-		gbc_btnNewButton.gridy = 0;
-		panel_6.add(btnNewButton, gbc_btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Cancelar");
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton_1.gridx = 2;
-		gbc_btnNewButton_1.gridy = 0;
-		panel_6.add(btnNewButton_1, gbc_btnNewButton_1);
 	}
 
 	/**
@@ -504,6 +285,11 @@ public class CardapioPanel extends JPanel {
 		panel_5.add(btnNewButton_2, gbc_btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Excluir");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removeCardapio();
+			}
+		});
 		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
 		gbc_btnNewButton_3.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnNewButton_3.gridx = 1;
@@ -634,9 +420,9 @@ public class CardapioPanel extends JPanel {
 		gbc_panel_6.gridy = 1;
 		panel_1.add(panel_6, gbc_panel_6);
 		GridBagLayout gbl_panel_6 = new GridBagLayout();
-		gbl_panel_6.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_panel_6.columnWidths = new int[]{0, 0, 0};
 		gbl_panel_6.rowHeights = new int[]{0, 0};
-		gbl_panel_6.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_6.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		gbl_panel_6.rowWeights = new double[]{0.0, Double.MIN_VALUE};
 		panel_6.setLayout(gbl_panel_6);
 		
@@ -659,26 +445,10 @@ public class CardapioPanel extends JPanel {
 		gbc_btnSalvar.gridy = 0;
 		panel_6.add(btnSalvar, gbc_btnSalvar);
 		
-		btnSessao = new JButton("Nova Sessão");
-		btnSessao.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Cardapio car = null;
-				JDialog d = new SessaoDialog(main, restaurante, car);
-				d.setVisible(true);
-			}
-		});
-		
-		GridBagConstraints gbc_BtnSessao = new GridBagConstraints();
-		gbc_BtnSessao.fill = GridBagConstraints.HORIZONTAL;
-		gbc_BtnSessao.insets = new Insets(0, 0, 0, 5);
-		gbc_BtnSessao.gridx = 1;
-		gbc_BtnSessao.gridy = 0;
-		panel_6.add(btnSessao, gbc_BtnSessao);
-		
 		btnCancelar = new JButton("Cancelar");
 		GridBagConstraints gbc_BtnCancelar = new GridBagConstraints();
 		gbc_BtnCancelar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_BtnCancelar.gridx = 2;
+		gbc_BtnCancelar.gridx = 1;
 		gbc_BtnCancelar.gridy = 0;
 		panel_6.add(btnCancelar, gbc_BtnCancelar);
 
